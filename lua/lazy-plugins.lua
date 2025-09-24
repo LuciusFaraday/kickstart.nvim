bcb5f8f8c34a437907f716393ca1c0f2ee8d6b65
Olzhas Sapargaliyev <solzhas@wtotem.com>
2025-09-25 00:01:20 +0500

    rework which-key config

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -8,20 +8,20 @@
 --  To update plugins you can run
 --    :Lazy update
 require('lazy').setup({
-  require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.blink-cmp',
-  require 'kickstart.plugins.conform',
-  require 'kickstart.plugins.debug',
   require 'kickstart.plugins.gitsigns',
+  require 'kickstart.plugins.telescope',
+  require 'kickstart.plugins.lspconfig',
+  require 'kickstart.plugins.conform',
+  require 'kickstart.plugins.blink-cmp',
+  require 'kickstart.plugins.tokyonight',
+  require 'kickstart.plugins.todo-comments',
+  require 'kickstart.plugins.mini',
+  require 'kickstart.plugins.treesitter',
+  require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
-  require 'kickstart.plugins.lspconfig',
-  require 'kickstart.plugins.mini',
+  require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
-  require 'kickstart.plugins.telescope',
-  require 'kickstart.plugins.todo-comments',
-  require 'kickstart.plugins.tokyonight',
-  require 'kickstart.plugins.treesitter',
   require 'kickstart.plugins.which-key',
 }, {
   ui = {
